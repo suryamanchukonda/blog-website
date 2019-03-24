db6814e9fa3a503c2dd2a78d7adf9ce4443965f6
suryamanchukonda <manchusurya@gmail.com>
2019-03-24 09:45:31 +0530

    some express route parameters

--- app.js
+++ app.js
@@ -53,6 +53,10 @@ app.post("/compose", function(req, res) {
   res.redirect("/");
 })
 
+app.get("/posts/:title", function(req, res){
+  console.log(req.params.title);
+})
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
